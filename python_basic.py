f1223e6091d42f6e98f77d5ffb39c639061e9a63
Gabriel <gabriel.leandrods@gmail.com>
2023-07-24 20:39:03 -0300

    incluindo pontuação no jogo

--- python_basic.py
+++ python_basic.py
@@ -4,10 +4,21 @@ print("**********************************")
 print("Bem vindo ao Jogo de adivinhação")
 print("**********************************")
 
-total_tentativas = 5
-numero_secreto = randrange(1, 10)
+numero_secreto = randrange(1, 101)
 print(numero_secreto)
 atual_tentiva = 0
+pontos = 1000
+
+print("Defina o nível de dificuldade: ")
+nivel = int(input("(1) Fácil (2) Médio (3) Difícil"))
+
+if nivel == 1:
+    total_tentativas = 10
+elif nivel == 2:
+    total_tentativas = 5
+else:
+    total_tentativas = 3
+
 
 for atual_tentiva in range(1, total_tentativas + 1):
 
@@ -28,8 +39,11 @@ for atual_tentiva in range(1, total_tentativas + 1):
 
     else:
         if maior:
-            print("Seu chute foi maior")
+            print("Seu chute foi maior ⬆️")
         else:
-            print("Seu chute foi menor")
+            print("Seu chute foi menor ⬇️")
+    pontos_perdidos = abs(numero_secreto - chute)
+    pontos -= pontos_perdidos
 
+print("Pontos: ", pontos)
 print("Game Over ☠")
