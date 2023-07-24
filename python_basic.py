c1fb4669be8ce3f62af0eb6778a5a2bc67ee9c21
Gabriel <gabriel.leandrods@gmail.com>
2023-07-24 11:04:11 -0300

    enviando arquivos do jogo

--- python_basic.py
+++ python_basic.py
@@ -1,4 +1,21 @@
-subst = "Python"
-verbo = "é"
-adjetivo = "fantástico"
-print(subst, verbo, adjetivo, sep="_", end="!\n")
+print("**********************************")
+print("Bem vindo ao Jogo de adivinhação")
+print("**********************************")
+
+numero_secreto = 10
+
+chute = int(input("Digite seu chute: "))
+
+print("Chute: ", chute)
+
+acertou = numero_secreto == chute
+maior = numero_secreto > chute
+
+
+if acertou:
+    print("Você acertou!")
+else:
+    if maior:
+        print("Seu chute foi maior")
+    else:
+        print("Seu chute foi menor")
