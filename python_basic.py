485fe7d498b3aae2dc91add22293d3188e59ba26
Gabriel <gabriel.leandrods@gmail.com>
2023-07-24 20:01:42 -0300

    numero randon implantado

--- python_basic.py
+++ python_basic.py
@@ -1,17 +1,24 @@
+from random import randrange
+
 print("**********************************")
 print("Bem vindo ao Jogo de adivinhação")
 print("**********************************")
 
 total_tentativas = 5
-numero_secreto = 10
+numero_secreto = randrange(1, 10)
+print(numero_secreto)
 atual_tentiva = 0
 
-while total_tentativas > atual_tentiva:
+for atual_tentiva in range(1, total_tentativas + 1):
 
-    atual_tentiva += 1
     print(f"tentativa {atual_tentiva} de {total_tentativas}")
     chute = int(input("Digite seu chute: "))
     print("Chute: ", chute)
+
+    if (chute < 1) or (chute > 100):
+        print("Digite um número entre 1 e 100:")
+        continue
+
     acertou = numero_secreto == chute
     maior = numero_secreto < chute
 
