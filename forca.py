459ba8a1a1d33efcd0ba02b10dd36e7488871911
Gabriel <gabriel.leandrods@gmail.com>
2023-07-25 22:48:56 -0300

    jogo da forca quase pronto

--- forca.py
+++ forca.py
@@ -1,5 +1,20 @@
 def forca():
     print("jogo da forca")
+    enforcou = False
+    acertou = False
+    palavra_secreta = "banana".upper()
+    acertos = []
+    while (not enforcou and not acertou):
+        chute = input("Qual letra?").upper()
+        index = 0
+
+        for letra in palavra_secreta:
+            if chute == letra:
+                print(f"Letra {letra} Posição:{index}")
+            index += 1
+            if acertos == palavra_secreta:
+                print("voce venceu")
+                break
 
 
 if __name__ == "__main__":
